1a2ad9bc5f8ba7a2b03daca5684e1f6e2f9f33ea
ALMA_DEV914 <aungonalna58@gamil.com>
2021-12-12 19:16:42 -0800

    add attachment

--- script.js
+++ script.js
@@ -4,9 +4,8 @@ function sendEmail(){
         To : "almabraun37582019@yahoo.com",
         From : document.getElementById("email").value,
         Subject : "New Contact Form Inquiry",
-        Body : "Name: " + document.getElementById("name").value + "<br> Email: " + document.getElementById("email").value + "<br> Phone No: " + document.getElementById("phone").value + "<br> Message: " + document.getElementById("message").value,
-        
         Attachments : [
+          "Name: " + document.getElementById("name").value + "<br> Email: " + document.getElementById("email").value + "<br> Phone No: " + document.getElementById("phone").value + "<br> Message: " + document.getElementById("message").value,
           {
             name : "smtpjs.png",
             path : "https://networkprogramming.files.wordpress.com/2017/11/smtpjs.png"
