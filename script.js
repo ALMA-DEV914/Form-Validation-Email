af4349763799ac6d8feaca371d761954074ed55d
ALMA_DEV914 <aungonalna58@gamil.com>
2021-12-12 19:32:23 -0800

    update the body of email

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function sendEmail(){
         To : "almabraun37582019@yahoo.com",
         From : document.getElementById("email").value,
         Subject : "New Contact Form Inquiry",
-        Attachments : [
-          "Name: " + document.getElementById("name").value + "<br> Email: " + document.getElementById("email").value + "<br> Phone No: " + document.getElementById("phone").value + "<br> Message: " + document.getElementById("message").value,
+        Body: "Name: " + document.getElementById("name").value + "<br> Email: " + document.getElementById("email").value + "<br> Phone No: " + document.getElementById("phone").value + "<br> Message: " + document.getElementById("message").value,
+        Attachments: [
           {
             name : "smtpjs.png",
             path : "https://networkprogramming.files.wordpress.com/2017/11/smtpjs.png"
